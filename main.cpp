c29192ad79eaed4610afe1a34da7de4ac94cfdf2
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2022-05-14 20:11:15 +0545

    Function convt to module

--- main.cpp
+++ main.cpp
@@ -1,58 +1,68 @@
 #include <iostream>
 using namespace std;
-void drawing(int position)
-{
-    switch(position)
-    {
-    case 1: 
-        cout << " ___________"<<endl;
-        cout << " |         }"<<endl;
-        cout << " |         " <<endl;
-        cout << "_|______________"<<endl; 
-    break;      
-    case 2:
-        cout << " ___________"<<endl;
-        cout << " |         }"<<endl;
-        cout << " |       \\  " <<endl;
-        cout << "_|______________"<<endl;
-    break;      
-    case 3:       cout << " ___________"<<endl;
-        cout << " |         }"<<endl;
-        cout << " |       \\ 0 " <<endl;
-        cout << "_|______________"<<endl;     
-    break;
-    case 4:       
-        cout << " ___________"<<endl;
-        cout << " |         }"<<endl;
-        cout << " |       \\ 0 /" <<endl;
-        cout << "_|______________"<<endl;
-    break;      
-    case 5:
-        cout << " ___________"<<endl;
-        cout << " |         }"<<endl;
-        cout << " |       \\ 0 /" <<endl;
-        cout << " |         |"<<endl;
-        cout << "_|______________"<<endl;
-    break;       
-    case 6:
-        cout << " ___________"<<endl;
-        cout << " |         }"<<endl;
-        cout << " |       \\ 0 /" <<endl;
-        cout << " |         |"<<endl;
-        cout << " |        /  "<<endl;
-        cout << "_|______________"<<endl;
-    break;       
-    case 7:  
-        cout << " ___________"<<endl;
-        cout << " |         }"<<endl;
-        cout << " |       \\ 0 /" <<endl;
-        cout << " |         |"<<endl;
-        cout << " |        / \\ "<<endl;
-        cout << "_|______________"<<endl;
-    }    
-}     
 
-int main(){
-    drawing(7);
+class Hangman
+{
+
+public:
+    void drawing(int position)
+    {
+        switch (position)
+        {
+        case 1:
+            cout << " ___________" << endl;
+            cout << " |         }" << endl;
+            cout << " |         " << endl;
+            cout << "_|______________" << endl;
+            break;
+        case 2:
+            cout << " ___________" << endl;
+            cout << " |         }" << endl;
+            cout << " |       \\  " << endl;
+            cout << "_|______________" << endl;
+            break;
+        case 3:
+            cout << " ___________" << endl;
+            cout << " |         }" << endl;
+            cout << " |       \\ 0 " << endl;
+            cout << "_|______________" << endl;
+            break;
+        case 4:
+            cout << " ___________" << endl;
+            cout << " |         }" << endl;
+            cout << " |       \\ 0 /" << endl;
+            cout << "_|______________" << endl;
+            break;
+        case 5:
+            cout << " ___________" << endl;
+            cout << " |         }" << endl;
+            cout << " |       \\ 0 /" << endl;
+            cout << " |         |" << endl;
+            cout << "_|______________" << endl;
+            break;
+        case 6:
+            cout << " ___________" << endl;
+            cout << " |         }" << endl;
+            cout << " |       \\ 0 /" << endl;
+            cout << " |         |" << endl;
+            cout << " |        /  " << endl;
+            cout << "_|______________" << endl;
+            break;
+        case 7:
+            cout << " ___________" << endl;
+            cout << " |         }" << endl;
+            cout << " |       \\ 0 /" << endl;
+            cout << " |         |" << endl;
+            cout << " |        / \\ " << endl;
+            cout << "_|______________" << endl;
+        }
+    }
+};
+int main()
+{
+    Hangman hangMan;
+    for (int i= 1; i<=7 ; i++){
+        hangMan.drawing(i);
+    }
     return 0;
 }
